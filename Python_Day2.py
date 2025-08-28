e9576440dcddf9500b662b7fa6908623c612173b
Daksh386 <psinghal651@gmail.com>
2025-08-28 14:50:05 +0530

    Add python_day2.ipynb

--- Python_Day2.py
+++
@@ -1,9 +0,0 @@
-#Data Type
-var1=1 #int data type
-print(type(var1))
-var2=True #bool data type
-print(type(var2))
-var3=10.023 #float data type
-print(type(var3))
-var=10+3j #complex data type
-print(type(var))
