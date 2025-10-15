dd58b3a56146ccbe71d433ec59dbdd2b3c1416eb
Daksh Singhal <psinghal651@gmail.com>
2025-10-15 09:12:41 +0530

    Work done

--- functions.py
+++ functions.py
@@ -1,24 +1,29 @@
-def calculategmean(a,b):
-    mean=(a/b)*(a+b)
-    print(mean)
+# def calculategmean(a,b):
+#     mean=(a/b)*(a+b)
+#     print(mean)
 
-def isgreater(a,b):
-    if(a>b):
-        print("First Number is greater")
-    else:
-        print("Second Number is greater")
+# def isgreater(a,b):
+#     if(a>b):
+#         print("First Number is greater")
+#     else:
+#         print("Second Number is greater")
 
-def islesser(a,b):
-    pass
+# def islesser(a,b):
+#     pass
 
 
 
-a=9
-b=8
-calculategmean(a,b)
-isgreater(a,b)
+# a=9
+# b=8
+# calculategmean(a,b)
+# isgreater(a,b)
 
-c=74
-d=90
-calculategmean(c,d)
-isgreater(c,d)
+# c=74
+# d=90
+# calculategmean(c,d)
+# isgreater(c,d)
+def sum(a,b):
+    return a+b
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print(sum(a,b))
