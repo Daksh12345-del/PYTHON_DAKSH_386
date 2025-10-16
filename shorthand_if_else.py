d28f41671e0b786ff8eb85058ec827ec7c526eb1
Daksh Singhal <psinghal651@gmail.com>
2025-10-16 12:57:44 +0530

    Short hand if else

---
+++ shorthand_if_else.py
@@ -0,0 +1,8 @@
+a=2
+b=330
+print("A") if a > b else print("B")
+print("A") if a > b else print("=") if a==b else print("B")
+#shorthand if-else statement ko ternary operator bhi kehte hain
+#syntax: print("A") if condition else print("B")
+c=9 if a>b else 0
+print(c)
