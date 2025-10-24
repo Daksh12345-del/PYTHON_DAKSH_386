aefb73d8c566c8a2b9adf7169292ec07c88f7ef2
Daksh Singhal <psinghal651@gmail.com>
2025-10-25 00:48:48 +0530

    Import learning done

---
+++ daksh.py
@@ -0,0 +1,3 @@
+def welcome():
+    print("Welcome to the import work module!")
+daksh=386
---
+++ import_work.py
@@ -0,0 +1,14 @@
+# import pandas
+# pandas.read_csv('data.csv') 
+# from math import floor, ceil
+# from math import * # Using wildcard (*) import to bring in all functions from math module
+# from math import factorial as f, ceil as c
+# a=f(3)
+# b=c(3.3)
+# print(a,b)
+# from daksh import welcome,daksh
+from daksh import *
+# print(dir(math))
+# print(math.nan,type(math.nan))
+welcome()
+print(daksh)
