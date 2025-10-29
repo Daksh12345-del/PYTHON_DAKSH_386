3faf14f956494a7f9f7ef35b63105afdd0293c72
Daksh Singhal <psinghal651@gmail.com>
2025-10-30 00:12:44 +0530

    Globsl and local variable

---
+++ global_localvariable.py
@@ -0,0 +1,14 @@
+x=4#global variable
+print (x)
+
+def hello():
+    global x
+    x=7
+    global y
+    y=5#local variable
+    print(f"The local y is still:{y}")
+    print(y)
+    print("Hello Daksh")
+hello()
+print(x)
+print(f"The global y is still:{y}")# This will give error as y is local variable and not defined globally
